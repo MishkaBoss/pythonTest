221324cb5d1f5fb7b9f0a7e4316a3b33b524531f
Michael Mozaikov <mozaikov60@gmail.com>
2024-02-19 10:50:36 +0200

    the commit message

--- test.py
+++ test.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/login')
+def login():
+    return '<h1>Login page</h1>'
+
 if __name__ == '__main__':
     app.run(debug=True)
